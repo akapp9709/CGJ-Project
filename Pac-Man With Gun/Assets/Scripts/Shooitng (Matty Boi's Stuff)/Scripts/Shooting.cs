b21dddf5c22ea2c77b40a6ed69950c9d85efd3c7
HobGob <matthias@adzoo.co.za>
2023-11-23 15:13:07 +0200

    Shooting Mechanic (basic, no damage)

---
+++ Pac-Man With Gun/Assets/Scripts/Shooitng (Matty Boi's Stuff)/Scripts/Shooting.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [Header("Player Stuff")]
+    public GameObject playerSprite;
+    public GameObject playerSpriteEyes;
+    public GameObject crosshairs;
+
+    [Header("Weapon Stuff")]
+    public GameObject weapon;
+    public GameObject weaponMuzzle;
+    public GameObject projectile_pistol;
+    public float pistoleForce;
+    [SerializeField] private Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Aiming();
+        Firing();
+    }
+
+    public void Aiming()
+    {
+        Vector3 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+        crosshairs.transform.position = mousePos;
+
+        Vector3 aimDirection = (mousePos - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+
+        weapon.transform.eulerAngles = new Vector3(0, 0, angle);
+        playerSpriteEyes.transform.eulerAngles = new Vector3(0, 0, angle);
+
+        Vector3 aimLocalScale = Vector3.one;
+        Vector3 playerLocalScale = Vector3.one;
+        Vector3 playerEyesLocalScale = Vector3.one;
+        if (angle > 90 || angle < -90)
+        {
+            aimLocalScale.y = -1f;
+            playerLocalScale.y = -3f;
+            playerLocalScale.x = -3f;
+
+            playerEyesLocalScale.y = -1f;
+        }
+        else
+        {
+            aimLocalScale.y = +1f;
+            playerLocalScale.y = +3f;
+            playerLocalScale.x = +3f;
+
+            playerEyesLocalScale.y = +1f;
+        }
+        weapon.transform.localScale = aimLocalScale;
+        playerSprite.transform.localScale = playerLocalScale;
+    }
+
+    public void Firing()
+    {
+        if(Input.GetMouseButtonDown(0) /*&& pistolEqipped*/)
+        {
+            GameObject projectileGO = Instantiate(projectile_pistol, weaponMuzzle.transform.position,
+            weaponMuzzle.transform.rotation);
+
+            Vector2 direction =  weaponMuzzle.transform.position - transform.position;
+
+            Rigidbody2D projectileRb = projectileGO.GetComponent<Rigidbody2D>();
+            projectileRb.AddForce(pistoleForce * direction);
+
+            //add damage dealer
+
+            Destroy(gameObject, 3f);
+        }
+        else if (Input.GetMouseButtonDown(0) /*&& rifleEqipped*/)
+        {
+            GameObject projectileGO = Instantiate(projectile_pistol, weaponMuzzle.transform.position,
+            weaponMuzzle.transform.rotation);
+
+            Vector2 direction = weaponMuzzle.transform.position - transform.position;
+
+            Rigidbody2D projectileRb = projectileGO.GetComponent<Rigidbody2D>();
+            projectileRb.AddForce(pistoleForce * direction);
+
+            //add damage dealer
+
+            Destroy(gameObject, 3f);
+        }
+    }
+}
