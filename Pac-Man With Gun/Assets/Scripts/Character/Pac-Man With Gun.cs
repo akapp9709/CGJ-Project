3bd9bd925976f01d5b46fc08cf74c6879fb455ef
akapp9709 <kappandrew2@gmail.com>
2023-11-23 14:41:01 +0200

    Platform Movement

--- Pac-Man With Gun/Assets/Scripts/Character/Pac-Man With Gun.cs
+++ Pac-Man With Gun/Assets/Scripts/Character/Pac-Man With Gun.cs
@@ -53,6 +53,15 @@ public partial class @PacManWithGun: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""f6187575-ad7f-4623-ac41-90b3b7100ecb"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -273,6 +282,28 @@ public partial class @PacManWithGun: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": ""XR"",
                     ""action"": ""Fire"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""f042422d-8f46-4bf9-b719-a28a4af65cfa"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""12724834-31b4-4742-a2d4-163e49c8f651"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -863,6 +894,7 @@ public partial class @PacManWithGun: IInputActionCollection2, IDisposable
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
         m_Player_Fire = m_Player.FindAction("Fire", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -939,6 +971,7 @@ public partial class @PacManWithGun: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Look;
     private readonly InputAction m_Player_Fire;
+    private readonly InputAction m_Player_Jump;
     public struct PlayerActions
     {
         private @PacManWithGun m_Wrapper;
@@ -946,6 +979,7 @@ public partial class @PacManWithGun: IInputActionCollection2, IDisposable
         public InputAction @Move => m_Wrapper.m_Player_Move;
         public InputAction @Look => m_Wrapper.m_Player_Look;
         public InputAction @Fire => m_Wrapper.m_Player_Fire;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -964,6 +998,9 @@ public partial class @PacManWithGun: IInputActionCollection2, IDisposable
             @Fire.started += instance.OnFire;
             @Fire.performed += instance.OnFire;
             @Fire.canceled += instance.OnFire;
+            @Jump.started += instance.OnJump;
+            @Jump.performed += instance.OnJump;
+            @Jump.canceled += instance.OnJump;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -977,6 +1014,9 @@ public partial class @PacManWithGun: IInputActionCollection2, IDisposable
             @Fire.started -= instance.OnFire;
             @Fire.performed -= instance.OnFire;
             @Fire.canceled -= instance.OnFire;
+            @Jump.started -= instance.OnJump;
+            @Jump.performed -= instance.OnJump;
+            @Jump.canceled -= instance.OnJump;
         }
 
         public void RemoveCallbacks(IPlayerActions instance)
@@ -1162,6 +1202,7 @@ public partial class @PacManWithGun: IInputActionCollection2, IDisposable
         void OnMove(InputAction.CallbackContext context);
         void OnLook(InputAction.CallbackContext context);
         void OnFire(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
     }
     public interface IUIActions
     {
--- Pac-Man With Gun/Assets/Scripts/Character/PlayerController.cs
+++ Pac-Man With Gun/Assets/Scripts/Character/PlayerController.cs
@@ -7,21 +7,26 @@ public class PlayerController : MonoBehaviour
 {
     private PacManWithGun _controls;
     private Vector2 direction;
+    private Rigidbody2D _rb;
 
-    [SerializeField] private float playerSpeed = 3;
+    [SerializeField] private float playerSpeed = 3, jumpForce = 10;
+    public bool _grounded;
 
     // Start is called before the first frame update
     void Start()
     {
         _controls = new PacManWithGun();
+        _rb = GetComponent<Rigidbody2D>();
         _controls.Player.Enable();
 
         _controls.Player.Move.performed += Move;
+        _controls.Player.Jump.started += Jump;
     }
 
     // Update is called once per frame
     void Update()
     {
+        _grounded = Physics2D.Raycast(transform.position, Vector2.down, 0.05f);
         var velocity = new Vector3(direction.x, direction.y, 0f) * playerSpeed * Time.deltaTime;
         transform.position += velocity;
     }
@@ -29,6 +34,14 @@ public class PlayerController : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         var inVec = context.ReadValue<Vector2>();
-        direction = inVec;
+        direction = new Vector2(inVec.x, 0f);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (_grounded)
+        {
+            _rb.AddForce(Vector3.up * jumpForce);
+        }
     }
 }
