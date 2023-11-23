1d95101312f9fccd4e411c91bea7ef48d42c0fc8
akapp9709 <kappandrew2@gmail.com>
2023-11-23 14:48:16 +0200

    Comments

--- Pac-Man With Gun/Assets/Scripts/Character/PlayerController.cs
+++ Pac-Man With Gun/Assets/Scripts/Character/PlayerController.cs
@@ -10,13 +10,17 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rb;
 
     [SerializeField] private float playerSpeed = 3, jumpForce = 10;
-    public bool _grounded;
+    private bool _grounded;
 
     // Start is called before the first frame update
     void Start()
     {
+        //New Input System Generated Class
         _controls = new PacManWithGun();
+        //Jump handled with Rigidbody
         _rb = GetComponent<Rigidbody2D>();
+
+        //Control delegate mapping
         _controls.Player.Enable();
 
         _controls.Player.Move.performed += Move;
@@ -26,19 +30,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Ground Check
         _grounded = Physics2D.Raycast(transform.position, Vector2.down, 0.05f);
+
+        // Velocity Manipulation
         var velocity = new Vector3(direction.x, direction.y, 0f) * playerSpeed * Time.deltaTime;
         transform.position += velocity;
     }
 
+    //Movement Method
     public void Move(InputAction.CallbackContext context)
     {
+        //Gets vector from player input
         var inVec = context.ReadValue<Vector2>();
+        // Allocates x-direction of input
         direction = new Vector2(inVec.x, 0f);
     }
 
+    //Jump Method
     public void Jump(InputAction.CallbackContext context)
     {
+        // only runs if player is grounded
         if (_grounded)
         {
             _rb.AddForce(Vector3.up * jumpForce);
